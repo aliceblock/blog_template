c5b6ea575b5b53cc0b6d53121301c25a52765b83
Saran Jenjirawat <registbacktown@gmail.com>
2014-11-26 12:32:08 +0700

    Remove slug from admin fields

--- admin.py
+++ admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from blog.models import Entry
 
 # Register your models here.
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+admin.site.register(Entry, EntryAdmin)
