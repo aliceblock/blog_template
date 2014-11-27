d9abf53449690125b5cb33a7f31ff223a59b7798
Saran Jenjirawat <registbacktown@gmail.com>
2014-11-27 23:34:06 +0700

    Add class to CommentForm and add empty list condition

--- forms.py
+++ forms.py
@@ -3,6 +3,11 @@ from django import forms
 from models import Comment
 
 class CommentForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    website = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    text = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control'}))
+
     class Meta:
         model = Comment
         exclude = ['entry']
