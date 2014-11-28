6de3041af9c9a2906c272122a288442bf9836ce8
Saran Jenjirawat <registbacktown@gmail.com>
2014-11-28 14:12:48 +0700

    Change required field in comments and temporary remove unused navbar.

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from models import Comment
 
 class CommentForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
-    website = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
+    website = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}), required=False)
     text = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control'}))
 
     class Meta:
